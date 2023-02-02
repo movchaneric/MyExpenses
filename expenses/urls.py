4c5de5664741d3414e3816938c79dd04b00db369
Eric M <ericm@Erics-MacBook-Air.local>
2023-02-02 22:23:06 +0200

    delete expense

--- expenses/urls.py
+++ expenses/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
 	#default route 
     path('', views.index, name = 'expenses'),
     path('add_expense/', views.add_expense, name = 'add_expense'),
+    path('edit_expense/<str:id>/', views.edit, name='edit_expense'),
+    path('delete_expense/<str:id>/', views.delete_expense, name='delete_expense')
+    
 
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -8,9 +8,8 @@ from .models import Expense
 
 @login_required(login_url='authentication/login/')
 def index(request):
-
 	expenses = Expense.objects.filter(owner=request.user)
-
+	categories = Category.objects.all()
 
 	context = {'expenses':expenses}
 	return render(request, 'expenses/index.html',context)
@@ -23,13 +22,19 @@ def add_expense(request):
 		amount = request.POST['amount']
 		# user havent enter an amount
 		if not amount:
-			messages.error(request, 'Amount field is required ')
-			# return render(request, 'expenses/add_expense.html',context)
+			messages.error(request, 'Amount field is required')
+		elif not amount.isnumeric():
+			messages.error(request, 'Amount must contain only digits')
+			return redirect('add_expense')
 		elif float(amount) <= 0:
-			messages.error(request, 'Amount has to be positive ')
-
+			messages.error(request, 'Amount has to be positive')
 
 		description = request.POST['description']
+		# Check if user updated description field
+		if not description:
+			messages.error(request, "Description field is required")
+   
+   
 		category = request.POST['category']
 		date = request.POST['date']
 
@@ -41,3 +46,54 @@ def add_expense(request):
 
 	context = {'categories':categories}
 	return render(request, 'expenses/add_expense.html',context)
+
+
+def edit(request,id):
+	# Get the expese data by expense id
+	expense = Expense.objects.get(pk=id)
+	categories = Category.objects.all()
+ 
+	if request.method == 'GET':		
+		context = {'expense':expense, 'values':expense, 'categories':categories}
+		return render(request, 'expenses/edit_expense.html', context)
+
+	if request.method == 'POST':
+		amount = request.POST['amount']
+		# check if user entered a new amount
+		if not amount:
+			messages.error(request, 'Amount field is required ')
+   
+		description = request.POST['description']
+		# Check if user updated description field
+		if not description:
+			messages.error(request, "Description field is required")
+   
+   
+		category = request.POST['category']
+		date = request.POST['date']
+  
+		# update selected expense 
+		expense.amount = amount
+		expense.date = date
+		expense.description = description
+		expense.category = category
+
+		# save an updated expense
+		expense.save()
+		
+		# Show message succes to user
+		messages.success(request, "Succesfuly updated expense")
+
+
+		context = {'expense':expense, 'values':expense, 'categories':categories}
+		return render(request, 'expenses/edit_expense.html', context)
+
+# Delete expense
+def delete_expense(request,id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, "Removed expense")
+    return redirect('expenses')
+
+
+
